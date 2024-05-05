b1fea79fe0c8c952372af77e8994b9e90bc3f7c6
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-05-05 16:18:41 +0300

    predeploy

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Game from './components/Game';
+import './styles/App.css';
+import './styles/styles.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='App-header'>
+        <Game/>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
